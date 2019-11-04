32228f931298fc6fb5d541182eb90a2abf26d1c0
allison cortez <allizetorc@gmail.com>
2019-11-04 03:13:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,5 +7,7 @@ class Anagram
   
   def match(array_of_words)
     array_of_words.select do |word|
-      (@word.split("").sort) == (word.split)
+      (@word.split("").sort) == (word.split("").sort)
+    end 
+  end
 end
